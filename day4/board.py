33972f85386a94370bd90ec1d55c6439ddc5fa47
lseppalainen <lauri@sellforte.com>
2021-12-04 13:56:06 +0200

    board to it's own script

--- day4/board.py
+++ day4/board.py
@@ -1,7 +1,5 @@
+"""Simple bingo board class"""
 import numpy as np
-# read bingo input
-with open('input', 'r') as f:
-    lines = f.readlines()
 
 
 class Board:
@@ -37,37 +35,3 @@ class Board:
     def __str__(self):
         out = str(self.numbers) + '\n' + str(self.marked)
         return out
-
-
-called_numbers = [int(x) for x in lines[0].split(',')]
-i = 1
-boards = []
-num_boards = (len(lines) - 1) // 6
-for bi in range(num_boards):
-    start_i = bi * 6 + 2
-    end_i = (bi + 1) * 6 + 1
-    rel_lines = lines[start_i:end_i]
-    nums = np.zeros((5, 5))
-    for ri in range(5):
-        nums[ri, :] = [int(x.strip()) for x in rel_lines[ri].split(' ') if x != '']
-    board = Board(5, nums)
-    boards.append(board)
-    # print(board)
-
-winner = False
-i = 0
-while not winner:
-    num = called_numbers[i]
-    for board in boards:
-        check = board.mark(num)
-        if check:
-            winner = board
-    i += 1
-print('Winning board:')
-print(winner)
-print(f'Score: {winner.score()}')
-print(f'Final output = {winner.score()} * {num} = ' +
-      f'{winner.score() * num}')
-
-
-# print(called_numbers)
---
+++ day4/part1.py
@@ -0,0 +1,41 @@
+import numpy as np
+import os
+import sys
+sys.path.append(os.getcwd())
+from board import Board
+# read bingo input
+with open('input', 'r') as f:
+    lines = f.readlines()
+
+called_numbers = [int(x) for x in lines[0].split(',')]
+i = 1
+boards = []
+num_boards = (len(lines) - 1) // 6
+for bi in range(num_boards):
+    start_i = bi * 6 + 2
+    end_i = (bi + 1) * 6 + 1
+    rel_lines = lines[start_i:end_i]
+    nums = np.zeros((5, 5))
+    for ri in range(5):
+        nums[ri, :] = [int(x.strip()) for x in rel_lines[ri].split(' ') if x != '']
+    board = Board(5, nums)
+    boards.append(board)
+    # print(board)
+
+winner = False
+i = 0
+while not winner:
+    num = called_numbers[i]
+    for board in boards:
+        check = board.mark(num)
+        if check:
+            winner = board
+    i += 1
+print('Winning board:')
+print(winner)
+print(f'Score: {winner.score()}')
+print(f'Final output = {winner.score()} * {num} = ' +
+      f'{winner.score() * num}')
+
+
+# print(called_numbers)
