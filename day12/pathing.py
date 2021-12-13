88a0f04a0df6d6f5b65b64306ca3e1c8b4e32fc2
lseppalainen <lauri@sellforte.com>
2021-12-13 17:52:57 +0200

    day 12 pt2 done

--- day12/pathing.py
+++ day12/pathing.py
@@ -1,3 +1,6 @@
+import time
+
+
 class Node:
 
     def __init__(self, name, neighbors, type):
@@ -17,13 +20,28 @@ class Node:
         return out
 
 
-path_input = """start-A
-start-b
-A-c
-A-b
-b-d
-A-end
-b-end
+path_input = """xx-end
+EG-xx
+iy-FP
+iy-qc
+AB-end
+yi-KG
+KG-xx
+start-LS
+qe-FP
+qc-AB
+yi-start
+AB-iy
+FP-start
+iy-LS
+yi-LS
+xx-AB
+end-KG
+iy-KG
+qc-KG
+FP-xx
+LS-qc
+FP-yi
 """.split('\n')
 
 
@@ -48,20 +66,20 @@ for path_pair in path_input:
             graph[b].add_neighbor(a)
         else:
             graph[b] = Node(b, {a}, node_type(b))
-[print(x) for x in graph.values()]
 
 
 def find_paths_single(graph, start_node):
-
+    # simple DFS
     paths = []
 
     def inner(graph, node, route):
-        if 'end' in route:
-            return
+        # add current node to route
         route += [node]
+        # if at the end, append path to path collection and stop
         if node == 'end':
-            # print(route)
             paths.append(route)
+            return
+        # otherwise, get valid neighbors and recursively call for each
         else:
             valid_visits = graph[node].get_neighbors()
             valid_visits = {x for x in valid_visits
@@ -69,23 +87,23 @@ def find_paths_single(graph, start_node):
             valid_visits = {x for x in valid_visits
                             if not ((graph[x].type == 'small')
                                     and (graph[x].name in route))}
-            # print(f'{node}: {valid_visits}')
             if valid_visits:
                 for n in valid_visits:
                     inner(graph, n, route.copy())
-                    # paths.append(inner(graph, n, route, paths))
             else:
+                # reached a dead end, return
                 return
     inner(graph, start_node, [])
     return paths
 
 
+start = time.time()
 paths = find_paths_single(graph, 'start')
-# print(paths)
-print(len(paths))
+end = time.time()
+print(f'Part 1 paths {len(paths)} (command ran for {end-start:.3f} s)')
 
 
-def count_small_apperances(graph, route):
+def count_small_appearances(graph, route):
     out = {}
     for n in route:
         if graph[n].type == 'small':
@@ -97,39 +115,34 @@ def count_small_apperances(graph, route):
 
 
 def find_paths_double(graph, start_node):
-
+    # overly complicated DFS
     paths = []
 
     def inner(graph, node, route):
-        if 'end' in route:
-            return
         route += [node]
         if node == 'end':
-            # print(route)
             paths.append(route)
+            return
         else:
             valid_visits = graph[node].get_neighbors()
             valid_visits = {x for x in valid_visits
                             if graph[x].type != 'start'}
             valid_visits = {x for x in valid_visits
                             if not ((graph[x].type == 'small')
-                                    and (route.count(graph[x].name) == 2))}
-            # print(f'{node}: {valid_visits}')
-            small_counts = count_small_apperances(graph, route)
-            print(small_counts)
+                                    and (route.count(graph[x].name) >= 1))}
+            # check if not yet visited any small cave twice on route
+            small_counts = count_small_appearances(graph, route)
             double_visit_available = 2 not in small_counts.values()
-            print(double_visit_available)
             single_visit = {}
+            # split neighbors into two classes
             if double_visit_available:
-                single_visit = {k for k in small_counts.keys()}
+                single_visit = {k for k in small_counts.keys()
+                                if k in graph[node].get_neighbors()}
                 valid_visits = {x for x in valid_visits if x not in
                                 single_visit}
-            print(f'{node}: {valid_visits}')
-            print(route)
             if valid_visits:
                 for n in valid_visits:
                     inner(graph, n, route.copy())
-                    # paths.append(inner(graph, n, route, paths))
             if single_visit:
                 for n in single_visit:
                     inner(graph, n, route.copy())
@@ -139,6 +152,8 @@ def find_paths_double(graph, start_node):
     return paths
 
 
+# so slowwwwwww
+start = time.time()
 paths = find_paths_double(graph, 'start')
-# print(paths)
-print(f'Part 2 paths {len(paths)}')
+end = time.time()
+print(f'Part 2 paths {len(paths)} (command ran for {end-start:.3f} s)')
